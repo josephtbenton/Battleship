4fa8278f4fa5e6961697835a4b7b95f061d12101
E-R-C <ehuynh.school@gmail.com>
2015-10-02 10:09:11 -0500

    Updated Controller to include Ferrer's messenger, and added server and target classes.

---
+++ battleship/game/Decoder.java
@@ -0,0 +1,10 @@
+package game;
+
+/**
+ * Created by Eric on 10/2/2015.
+ */
+public class Decoder {
+    private String messageType;
+    private boolean hit;
+    private boolean isMessage;
+}
--- battleship/net/Sender.java
+++
@@ -1,7 +0,0 @@
-package net;
-
-/**
- * Created by josephbenton on 9/30/15.
- */
-public class Sender {
-}

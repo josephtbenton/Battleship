a05b84d793aaed04daeea917b823380cdf614590
Rmershon <MershonRB@hendrix.edu>
2015-10-03 14:37:58 -0500

    New Board Interface

---
+++ battleship/game/Board.java
@@ -0,0 +1,10 @@
+package game;
+
+/**
+ * Created by reedmershon on 10/3/15.
+ */
+public interface Board {
+    public boolean registerHit(Coordinate location);
+    public boolean registerMiss(Coordinate location);
+    public boolean registerAttack(Coordinate location);
+}

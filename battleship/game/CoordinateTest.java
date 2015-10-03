ffe27fc4accbe5acf97342c36586efcb3582a028
GerryJarcia <tschiemerbr@hendrix.edu>
2015-10-03 16:47:37 -0500

    Fixed initializing the coordinate variables

--- battleship/game/CoordinateTest.java
+++ battleship/game/CoordinateTest.java
@@ -6,73 +6,56 @@ public class CoordinateTest {
 
     @org.junit.Test
     public void testToString() throws Exception {
-        Coordinates coor = new Coordinates();
-        coor.x = 3;
-        coor.y = 4;
-        AsserEquals("Should convert 3,4 to D5", "D5", coor.toString());
-        
-        coor.x = 18;
-        coor.y = -4;
-        AsserEquals("Coordinates 18,-4 are out of bounds, so it will not convert", "18-4", coor.toString());
+        Coordinates coor = new Coordinates(3, 4);
+        AssertEquals("Should convert 3,4 to D5", "D5", coor.toString());
+        Coordinates coor2 = new Coordinates(18, -4);
+        AssertEquals("Coordinates 18,-4 are out of bounds, so it will not convert", "18-4", coor.toString());
 
     }
 
     @org.junit.Test
     public void testEquals() throws Exception {
-        Coordinates coor1 = new Coordinates();
-        Coordinates coor2 = new Coordinates();
-        coor1.x = 8;
-        coor1.y = 3;
-        coor2.x = 7;
-        coor2.y = 9;
-        AsserFalse("coor1 and coor2 are not at the same location, so false should be returned", coor1.equals(coor2));
-        coor2.x = 8;
-        coor2.y = 3;
-        AsserFalse("coor1 and coor2 are now at the same location, so true should be returned", coor1.equals(coor2));
+        Coordinates coor1 = new Coordinates(8, 3);
+        Coordinates coor2 = new Coordinates(7, 9);
+        AssertFalse("coor1 and coor2 are not at the same location, so false should be returned", coor1.equals(coor2));
+        Coordinates coor3 = new Coordinates(8, 3);
+        AssertTrue("coor1 and coor3 are at the same location, so true should be returned", coor1.equals(coor3));
 
     }
 
     @org.junit.Test
     public void testGetX() throws Exception {
-        Coordinates coor = new Coordinates();
-        coor.x = 6;
-        coor.y = 2;
-        AsserEquals("Should return just the int for x, which is 6", 6, coor.getX();
-        coor.x = 8;
-        AsserEquals("Should return just the int for x, which is 8", 8, coor.getX();
-        coor.y = 7;
-        AsserEquals("Should return just the int for x, which is 8", 8, coor.getX();
+        Coordinates coor1 = new Coordinates(6, 2);
+        AssertEquals("Should return just the int for x, which is 6", 6, coor1.getX();
+        Coordinates coor2 = new Coordinates(8, 3);
+        AssertEquals("Should return just the int for x, which is 8", 8, coor2.getX();
+        Coordinates coor3 = new Coordinates(1, 7);
+        AssertEquals("Should return just the int for x, which is 1", 1, coor3.getX();
 
     }
 
     @org.junit.Test
     public void testGetY() throws Exception {
-        Coordinates coor = new Coordinates();
-        coor.x = 6;
-        coor.y = 2;
-        AsserEquals("Should return just the int for y, which is 2", 2, coor.getY();
-        coor.y = 5;
-        AsserEquals("Should return just the int for y, which is 5", 5, coor.getY();
-        coor.x = 7;
-        AsserEquals("Should return just the int for y, which is 5", 5, coor.getY();
+        Coordinates coor1 = new Coordinates(6, 2);
+        AssertEquals("Should return just the int for y, which is 2", 2, coor1.getY();
+        Coordinates coor2 = new Coordinates(6, 5);
+        AssertEquals("Should return just the int for y, which is 5", 5, coor2.getY();
+        Coordinates coor3 = new Coordinates(7, 5);
+        AssertEquals("Should return just the int for y, which is 5", 5, coor3.getY();
 
     }
 
     @org.junit.Test
     public void testIsLegal() throws Exception {
-        Coordinates coor = new Coordinates();
-        coor.x = 6;
-        coor.y = 2;
-        AssertTrue("Should return that the coordinates are legal", coor.isLegal();
-        coor.x = 1;
-        coor.y = 5;
-        AssertTrue("Should return that the coordinates are legal", coor.isLegal();
-        coor.x = 7;
-        coor.y = 13;
-        AssertFalse("Should return that the coordinates are not legal", coor.isLegal();
-        coor.x = -2;
-        coor.y = 8
-        AssertTrue("Should return that the coordinates are legal", coor.isLegal();
-
+        Coordinates coor1 = new Coordinates(6, 2);
+        AssertTrue("Should return that the coordinates are legal", coor1.isLegal();
+        Coordinates coor2 = new Coordinates(1, 5);
+        AssertTrue("Should return that the coordinates are legal", coor2.isLegal();
+        Coordinates coor3 = new Coordinates(7, 13);
+        AssertFalse("Should return that the coordinates are not legal", coor3.isLegal();
+        Coordinates coor4 = new Coordinates(-2, 8);
+        AssertFalse("Should return that the coordinates are not legal", coor4.isLegal();
+        Coordinates coor5 = new Coordinates(-5, 27);
+        AssertFalse("Should return that the coordinates are not legal", coor5.isLegal();
     }
 }

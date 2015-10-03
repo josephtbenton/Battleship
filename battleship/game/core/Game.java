86a8ce4088088f42872df2539756bbb3e2d6bd59
josephtbenton <josephtbenton@gmail.com>
2015-10-03 16:29:34 -0500

    Implemented getMessage method in Game.java

--- battleship/game/core/Game.java
+++ battleship/game/core/Game.java
@@ -1,7 +1,28 @@
 package game.core;
 
+import game.Board;
+import game.Message;
+import game.Radar;
+import game.ShipBoard;
+import net.MessageType;
+
 /**
  * Created by josephbenton on 10/3/15.
  */
 public class Game {
+    Radar radar;
+    ShipBoard shipBoard;
+
+    public void getMessage(String incoming) {
+        Message message = new Message(incoming);
+        if (message.getType() == MessageType.ATTACK) {
+            shipBoard.registerAttack(message.getCoordinate());
+        } else if (message.getType() == MessageType.HIT) {
+            radar.registerHit(message.getCoordinate());
+        } else if (message.getType() == MessageType.MISS) {
+            radar.registerMiss(message.getCoordinate())
+        } else if (message.getType() == MessageType.TEXT) {
+            //print message somewhere
+        }
+    }
 }
